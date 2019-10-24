87d26307cbf9c0292ff2df852ed7f3821b0c74b6
YMG <y.munizgonzalez@gmail.com>
2019-10-24 12:29:11 -0300

    Primeiro commit

---
+++ src/database/index.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+import dataBaseConfig from '../config/database';
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
+
+const models = [User, File, Appointment];
+
+class Database {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.connection = new Sequelize(dataBaseConfig);
+        models
+            .map(model => model.init(this.connection))
+            .map(
+                model =>
+                    model.associate && model.associate(this.connection.models)
+            );
+    }
+}
+
+export default new Database();
