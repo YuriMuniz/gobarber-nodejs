126a9bb278d4065e55fc8fb11853ba751d327874
YMG <y.munizgonzalez@gmail.com>
2019-10-24 21:56:39 -0300

    feat: Configuração mongodb e criação de schema Notification

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import dataBaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -9,6 +10,7 @@ const models = [User, File, Appointment];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -19,6 +21,17 @@ class Database {
                 model =>
                     model.associate && model.associate(this.connection.models)
             );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://192.168.99.100:27017/gobarber',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: true,
+                useUnifiedTopology: true,
+            }
+        );
     }
 }
 
