c15f3b406ec6d0b1c2d02561c098edfd98444e39
YMG <y.munizgonzalez@gmail.com>
2019-10-29 19:20:25 -0300

    feat: configuração das variáveis de ambiente.

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
     secure: false,
     auth: {
-        user: 'bbd9c4df0aeeab',
-        pass: '48294320de1fd7',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     },
     default: {
         from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-    dsn: 'https://839fa5d468c349c5b321b9ab2a8bcc26@sentry.io/1801312',
+    dsn: process.env.SENTRY_DSN,
 };
